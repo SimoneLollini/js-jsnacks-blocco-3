07e2c1d8a66e81cf830ba6d3e8d67b442fc4cd70
SimoneLollini <lollinisimone@gmail.com>
2022-10-21 12:16:00 +0200

    Converted the first letter to uppercase

---
+++ js-snack-2/main.js
@@ -0,0 +1,19 @@
+// Snack 2
+// A partire da un array di stringhe, crea un secondo array formattando le stringhe del primo array in minuscolo e con l’iniziale maiuscola.
+// Es: [‘pippo’, ‘PLUTO’, ‘Paperino’] => [‘Pippo’, ‘Pluto’, ‘Paperino’]
+
+
+nomi = ['pippo', 'PLUTO', 'Paperino']
+
+
+
+const nomiLower = nomi.map(nome => {
+    console.log(nome);
+    return nome.toLowerCase()
+});
+console.log(nomiLower);
+
+const firtChart = nomiLower.map(nome => {
+    return nome.charAt(0).toUpperCase();
+})
+console.log(firtChart);
